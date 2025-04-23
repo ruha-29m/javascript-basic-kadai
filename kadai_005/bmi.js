cf0a94890889dd69ffd68b24ecacb54a70b41742
ruha-29m <haruka.a.kick@gmail.com>
2025-04-23 21:39:06 +0900

    コードを修正

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -6,7 +6,7 @@ let userName= "体重,身長";
 
 let userHeight=1.7;
 
-    userBmi=userWeight/(userHeight**2);
+let userBmi=userWeight/(userHeight**2);
 
 console.log(userName + "のBMIは" + userBmi.toFixed(2) + "です。");
 console.log("BMIの計算式は、体重(kg) ÷ 身長(m)² です。");
