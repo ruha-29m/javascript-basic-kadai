3a8b83c5e89261006c87de1c8f4a21a9696a49c8
ruha-29m <haruka.a.kick@gmail.com>
2025-04-23 21:20:44 +0900

    html_jsのファイルを作成

---
+++ kadai_005/bmi.js
@@ -0,0 +1,12 @@
+
+
+let userName= "体重,身長";
+
+    userWeight=68;
+
+let userHeight=1.7;
+
+    userBmi=userWeight/(userHeight**2);
+
+console.log(userName + "のBMIは" + userBmi.toFixed(2) + "です。");
+console.log("BMIの計算式は、体重(kg) ÷ 身長(m)² です。");
