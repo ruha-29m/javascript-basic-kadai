0fff6f98988c00f84162457db69a4712fdb2460a
ruha-29m <haruka.a.kick@gmail.com>
2025-04-28 00:59:42 +0900

    修正4

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -4,6 +4,6 @@ var year = today.getFullYear();
 var month = today.getMonth() + 1; // getMonth() returns 0-11
 var day = today.getDate();
 
-        console.log( year + '年' + month + '月' + day + '日' );
+    console.log( year + '年' + month + '月' + day + '日' );
 
 
